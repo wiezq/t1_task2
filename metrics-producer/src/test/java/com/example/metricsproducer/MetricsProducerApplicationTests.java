226c91f054c9305335f5d24b695e641eddc15b0c
Ivan Maltsev <53794764+wiezq@users.noreply.github.com>
2024-03-18 11:37:00 +0300

    Add files via upload

---
+++ metrics-producer/src/test/java/com/example/metricsproducer/MetricsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metricsproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
